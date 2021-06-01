c010bd64aa94683f50036ab5bf03214a2a14eba4
Thabo Matjuda <tl.matjuda@gmail.com>
2021-06-01 18:46:34 +0200

    Initial commit

---
+++ src/main/kotlin/com/toob/koot/springbootkotlinmockito/SpringBootKotlinMockitoApplication.kt
@@ -0,0 +1,11 @@
+package com.toob.koot.springbootkotlinmockito
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinMockitoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootKotlinMockitoApplication>(*args)
+}
---
+++ src/test/kotlin/com/toob/koot/springbootkotlinmockito/SpringBootKotlinMockitoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toob.koot.springbootkotlinmockito
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinMockitoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
